d4d8e619fbfc0b82ecaae27a899b18ec539cf27d
Alade Toheeb <aladetoheeb5@gmail.com>
2022-07-03 00:24:46 +0100

    Update models

---
+++ I4G013521HTC/links/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+# Create your models here.
+
+class Link(models.Model):
+    target_url = models.URLField(max_length=200)
+    description= models.CharField(max_length=200)
+    identifier = models.SlugField(max_length=20, unique=True, blank=True)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now)
+    active = models.BooleanField(default=True)
